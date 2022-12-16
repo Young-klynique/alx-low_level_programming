824c3a05a5311b33da158c2ad0bd35ce457547c2
Samuel Young Appiah <appiahyoung2002@gmail.com>
2022-12-16 09:48:34 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /**
  * main - prints a given phrase
  * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	puts("\"Programming is like building a multiligual puzzle");
+	return (0);
 }
