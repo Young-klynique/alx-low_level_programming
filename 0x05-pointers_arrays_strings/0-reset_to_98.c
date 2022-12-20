df3882874525dd3bacc16cd17459fbe9b0851434
Samuel Young Appiah <appiahyoung2002@gmail.com>
2022-12-20 10:56:46 -0800

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,11 +4,11 @@
  * reset_to_98 - function that takes a pointer to an int as parameter and 
  * updates the value it points to to 98.
  *
- * @n:This is the entry 
+ * @n: This is the entry 
  *
  */
 
-void reset_to-98(int *n)
+void reset_to_98(int *n)
 {
 	*n = 98;
 }
